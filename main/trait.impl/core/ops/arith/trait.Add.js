e7f00f6c443b592008ff0575eb2a25b34434a282
kl-botsu <botsu@khonsulabs.com>
2024-09-22 15:57:47 +0000

    Deploying to gh-pages from @ khonsulabs/approximint@c654d64a6bd7f415c52c0416a215e886d4f52d1b 🚀

--- main/trait.impl/core/ops/arith/trait.Add.js
+++ main/trait.impl/core/ops/arith/trait.Add.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["approximint",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for <a class=\"struct\" href=\"approximint/struct.Approximint.html\" title=\"struct approximint::Approximint\">Approximint</a>"]]]]);
+    var implementors = Object.fromEntries([["approximint",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for <a class=\"struct\" href=\"approximint/struct.Approximint.html\" title=\"struct approximint::Approximint\">Approximint</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.i32.html\">i32</a>&gt; for <a class=\"struct\" href=\"approximint/struct.Approximint.html\" title=\"struct approximint::Approximint\">Approximint</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[291]}
+//{"start":57,"fragment_lengths":[669]}
--- main/trait.impl/core/ops/arith/trait.AddAssign.js
+++ main/trait.impl/core/ops/arith/trait.AddAssign.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["approximint",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a> for <a class=\"struct\" href=\"approximint/struct.Approximint.html\" title=\"struct approximint::Approximint\">Approximint</a>"]]]]);
+    var implementors = Object.fromEntries([["approximint",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a> for <a class=\"struct\" href=\"approximint/struct.Approximint.html\" title=\"struct approximint::Approximint\">Approximint</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.i32.html\">i32</a>&gt; for <a class=\"struct\" href=\"approximint/struct.Approximint.html\" title=\"struct approximint::Approximint\">Approximint</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[309]}
+//{"start":57,"fragment_lengths":[705]}
